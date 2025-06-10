24061f369ea22ba687c929a760b68bd3351c37d7
Carl0gonzalez <carlogonzalez@casadelsoftware.com>
2025-06-09 22:20:42 -0400

    actualizando AppTest.java

--- src/test/java/com/equipo/taskmaster/AppTest.java
+++ src/test/java/com/equipo/taskmaster/AppTest.java
@@ -1,38 +1,15 @@
 package com.equipo.taskmaster;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import org.junit.Test;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+public class AppTest {
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void testAddTask() {
+        App.tasks.clear();
+        App.addTask("Terminar ejercicio Maven");
+        assertEquals(1, App.tasks.size());
     }
 }
