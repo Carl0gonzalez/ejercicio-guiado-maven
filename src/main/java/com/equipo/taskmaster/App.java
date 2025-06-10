deab1b4a68792edf98e6d9c83deb5558e8626ac9
Carl0gonzalez <carlogonzalez@casadelsoftware.com>
2025-06-09 22:19:56 -0400

    actualizando app.java

--- src/main/java/com/equipo/taskmaster/App.java
+++ src/main/java/com/equipo/taskmaster/App.java
@@ -1,13 +1,26 @@
 package com.equipo.taskmaster;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.ArrayList;
+
+public class App {
+    public static ArrayList<String> tasks = new ArrayList<>();
+
+    public static void main(String[] args) {
+        System.out.println("Bienvenido a TaskMaster!");
+        addTask("Estudiar Maven");
+        addTask("Leer sobre CI/CD");
+        printTasks();
+        System.out.println("Ambiente: " + System.getProperty("env.name"));
+    }
+
+    public static void addTask(String task) {
+        tasks.add(task);
+    }
+
+    public static void printTasks() {
+        System.out.println("Tareas pendientes:");
+        for (String t : tasks) {
+            System.out.println("- " + t);
+        }
     }
 }
